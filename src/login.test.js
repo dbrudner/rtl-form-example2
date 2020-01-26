6d10fe76a89c041df3deba1b98205cf6b9df3ff8
Dave Brudner <dbrudne1@gmail.com>
2020-01-26 11:48:08 -0600

    1: adding initial render test (test should fail)

--- src/login.test.js
+++ src/login.test.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { render, fireEvent, wait } from "@testing-library/react";
-import { Login } from "./login";
+import React from 'react';
+import { render, fireEvent, wait } from '@testing-library/react';
+import { Login } from './login';
 
 const mockResponse = {
-	user: "1",
-	pass: "1",
+	user: '1',
+	pass: '1'
 };
 
 global.fetch = jest.fn(() => {
 	return new Promise(() => ({
 		json: () => mockResponse,
-		ok: true,
+		ok: true
 	}));
 });
 
-describe("Login", () => {
+describe('Login', () => {
 	let Subject;
 	const updateCurrentUser = jest.fn();
 
@@ -22,27 +22,7 @@ describe("Login", () => {
 		Subject = render(<Login updateCurrentUser={updateCurrentUser} />);
 	});
 
-	it("should render", () => {
+	it('should render', () => {
 		expect(Subject).toBeTruthy();
-	});
-
-	it("should login with correct user and pass", () => {
-		const { getByLabelText, getByText } = Subject;
-
-		const userInput = getByLabelText("Username");
-		const passwordInput = getByLabelText("Pass");
-
-		fireEvent.change(userInput, { target: "1" });
-		fireEvent.change(passwordInput, { target: "1" });
-
-		const loginButton = getByText("Login");
-
-		fireEvent.click(loginButton);
-
-		expect(fetch).toHaveBeenCalled();
-
-		wait(() => {
-			expect(updateCurrentUser).toHaveBeenCalledWith(mockResponse);
-		});
 	});
 });
